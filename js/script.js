03138ac4af96bda65fcf588589d01ece24f41110
Denis Levy de M. Cravo <62116847+DenisLevy76@users.noreply.github.com>
2020-09-23 00:09:52 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+function sortear(){
+   
+   let nSorteados = document.getElementById('n').value
+   const min = Number(document.querySelector('#min').value)
+   const max = Number(document.querySelector('#max').value)
+   const organizar = document.querySelector('#organizar').checked
+   const resultado = document.querySelector('#valor')
+   const resultados = []
+
+   if (nSorteados <= 0){
+      nSorteados = 1
+   }
+
+   while (resultados.length < nSorteados){
+      let numSorteado = Math.floor(Math.random() * (max - min + 1)) + min
+
+      if (resultados.indexOf(numSorteado) == -1){
+         resultados.push(numSorteado)
+      }
+   }
+
+   if (organizar === true){
+      resultados.sort(sorter)
+   }
+
+   if (resultados.length == 1){
+      resultado.style.opacity = '100%'
+      resultado.innerHTML = `O número escolhido foi:<br>${resultados}`
+   }
+
+   else{
+      resultado.style.opacity = '100%'
+      resultado.innerHTML = `Os números escolhidos foram:<br>${resultados}`
+   }
+
+}
+
+function sorter(a, b) {
+   if (a < b) return -1;
+   if (a > b) return 1;
+   return 0;
+}
